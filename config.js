b9d94a33c23ab1e5b778d0f24fa8aea025cff860
Xoul ser <131429508+Xoulfff@users.noreply.github.com>
2024-07-12 19:00:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ module.exports = {
     REJECT_CALLS: convertToBool(process.env.REJECT_CALLS) || false,
     PMB: process.env.PMB || '_Personal messages not allowed, BLOCKED!_',
     READ_COMMAND: convertToBool(process.env.READ_COMMAND) || true,
-    SESSION: (process.env.SESSION || process.env.SESSION_ID || 'Raganork~8f53e0c9fef3d256acaace487f483f2f:446b3768747a672f6b386b3d').trim() || '',
+    SESSION: (process.env.SESSION || process.env.SESSION_ID || 'Raganork~698132870361d949d1d0a0102566742b:7a564b627568562b2f33673d').trim() || '',
     IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
     RG: process.env.RG || '919074309534-1632403322@g.us,120363116963909366@g.us',
     BOT_INFO: process.env.BOT_INFO || '𝐗ᴏᴜᴋ 𝐌ᴅ;𝐗ᴏᴜʟ 𝐒ᴇʀ;0;https://i.imgur.com/CoAcgTb.jpeg;https://chat.whatsapp.com/BxR0LrCkAVKGHriVvo7sKy',
